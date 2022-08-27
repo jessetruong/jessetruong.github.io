3c91248dde841b9ff4707d17810580299ee192d1
Jesse Truong <jessetruong@gmail.com>
2022-08-27 16:30:13 -0400

    Loop around navigation working

--- script.js
+++ script.js
@@ -5,12 +5,15 @@ http.send();
 
 document.onkeydown = handleKeyNavigation;
 
-let focusedSet = -1;
-let focusedImage = -1;
+let focusedSet = 0;
+let focusedImage = 0;
+
+let selectedImagePosition = 0;
+let beginRowScrolling = false;
 
 let imagesPerPage;
 
-window.addEventListener('resize', handleWindowResize)
+window.addEventListener('resize', handleWindowResize);
 window.onload = handleWindowOnLoad;
 
 // Disable mouse navigation
@@ -28,35 +31,34 @@ function handleWindowResize() {
 }
 
 function handleKeyNavigation(e) {
+    let horizonalIncrement = 0;
+    let verticalIncrement = 0;
     switch (e.keyCode) {
         case 37:
-            console.log('left')
-            cleanCurrentSelection();
-            focusedImage--;
-            navigateToNewSelection();
+            horizonalIncrement--;
             break;
         case 38:
-            console.log('up')
-            cleanCurrentSelection();
-            focusedSet--;
-            navigateToNewSelection();
+            verticalIncrement--;
             break;
         case 39:
-            console.log('right')
-            cleanCurrentSelection();
-            focusedImage++;  
-            navigateToNewSelection();      
+            horizonalIncrement++;   
             break;
         case 40:
-            console.log('down')
-            cleanCurrentSelection();
-            focusedSet++;
-            navigateToNewSelection();
+            verticalIncrement++;
             break;
         default:
             break;
     }
+
+    if (e.keyCode >= 37 && e.keyCode <= 40) {
+        cleanCurrentSelection();
+        focusedSet += verticalIncrement;
+        focusedImage += horizonalIncrement;
+        selectedImagePosition += horizonalIncrement;
+        focusOnNewSelection(horizonalIncrement, verticalIncrement);
+    }
 }
+
 
 function cleanCurrentSelection() {
     if (focusedSet >= 0 && focusedImage >= 0) {
@@ -66,42 +68,90 @@ function cleanCurrentSelection() {
     }
 }
 
-function navigateToNewSelection() {
-
-    let scrollWidth = 0;
-    focusedSet = Math.min(Math.max(0, focusedSet), 3);
+function focusOnNewSelection(horizonalIncrement, verticalIncrement) {
+    const numTotalSets = document.querySelectorAll('.set-container').length; 
+    const contentSection = document.querySelector('#content-selector');
+    if (focusedSet < 0) {
+        contentSection.prepend(contentSection.lastChild);
+        focusedSet = numTotalSets - 1;
+    } else if (focusedSet >= numTotalSets) {
+        contentSection.append(contentSection.firstChild);
+        focusedSet = 0;
+    }
 
     const setImagesContainer = document.getElementById(`set_${focusedSet}_container`);
-    const images = document.getElementById(`set_${focusedSet}_container`).querySelector('.set-images');
+    setImagesContainer.querySelector('.left-arrow').style.display = 'block';
+    setImagesContainer.querySelector('.right-arrow').style.display = 'block';
 
-    focusedImage = Math.min(Math.max(0, focusedImage), images.children.length - 1);
+    const images = document.getElementById(`set_${focusedSet}_container`).querySelector('.set-images');
     
-    if(focusedImage > imagesPerPage - 1) {
-        setImagesContainer.querySelector('.left-arrow').style.display = 'block';
-    } else {
-        setImagesContainer.querySelector('.left-arrow').style.display = 'none';
+    if (selectedImagePosition < 0 || selectedImagePosition >= Math.ceil(imagesPerPage/2)) {
+        beginRowScrolling = true;
     }
-    if(focusedImage < images.children.length - 1) {
-        setImagesContainer.querySelector('.right-arrow').style.display = 'block';
-    } else {
-        setImagesContainer.querySelector('.right-arrow').style.display = 'none';
+
+    // const index = Array.from(images.children).indexOf(document.getElementById(`set_${focusedSet}_image_${focusedImage}`) );
+    // if (!images.children[index + Math.ceil(imagesPerPage/2)]) {
+    //     images.prepend(createPlaceholderImage());
+    //     console.log('before', index + Math.ceil(imagesPerPage/2), images.children[index + Math.ceil(imagesPerPage/2)])
+    //     images.appendChild(images.querySelector('.preview-image'));
+    //     console.log('after', index + Math.ceil(imagesPerPage/2), images.children[index + Math.ceil(imagesPerPage/2)])
+    // }
+
+    if (focusedImage < 0) {
+        focusedImage = images.querySelectorAll('.preview-image').length - 1;
+        // while (images.querySelectorAll('.preview-image')[selectedImagePosition].getAttribute('id') !== `set_${focusedSet}_image_${focusedImage}`) {
+        //     console.log(images.querySelectorAll('.preview-image')[selectedImagePosition].getAttribute('id'), `set_${focusedSet}_image_${focusedImage}`);
+        //     images.prepend(images.lastChild);
+        // }
+    } else if (focusedImage > images.querySelectorAll('.preview-image').length - 1) {
+        focusedImage = 0;
     }
-        // setContainerImages.forEach(image => {
-            
-            console.log(images.children.length)
-            images.addEventListener('animationend', () => {
-                console.log('Animation end');
-                images.classList.add('scrolled');
-                images.classList.remove('scroll-image')
-            });
-            const currentScrollWidth = parseInt(getComputedStyle(images).getPropertyValue('--scroll-width'), 10) || 0;
-            images.style.setProperty('--current-scroll-position',  currentScrollWidth + 'px')
-            scrollWidth = Math.max(focusedImage - imagesPerPage + 1, 0) * 320
-            images.style.setProperty('--scroll-width',  -scrollWidth + 'px')
-            images.classList.add('scroll-image');
-        // })
+    
+    if (beginRowScrolling) {
+        let currentScrollPosition = parseInt(getComputedStyle(images).getPropertyValue('--scroll-distance'), 10) || 0;
+        images.style.setProperty('--current-scroll-position',  currentScrollPosition + 'px');
+        
+        const scrollAnimationFinished = () => {
+            images.classList.add('scrolled');
+            images.classList.remove('scrolling-image');
+            images.removeEventListener('animationend', scrollAnimationFinished);
+        }
+        images.addEventListener('animationend', scrollAnimationFinished);
+        
+
+        console.log('Selected image position', selectedImagePosition);
+        scrollDistance = focusedImage <= Math.floor(imagesPerPage/2) ? 0 : (Math.floor(imagesPerPage/2) - focusedImage) * 320;
+
+
+        if (focusedImage === images.querySelectorAll('.preview-image').length - 2 && horizonalIncrement === -1) {
+            currentScrollPosition = (Math.floor(imagesPerPage/2) - focusedImage - 1) * 320;
+            images.style.setProperty('--current-scroll-position',  currentScrollPosition + 'px');
+        }
+
+        // Loop backward from the beginning to the end
+        if (selectedImagePosition < 0) {
+            scrollDistance = -390;
+        }
+        
+        console.log('Scroll distance', currentScrollPosition, (selectedImagePosition - imagesPerPage), scrollDistance);
+        images.style.setProperty('--scroll-distance',  scrollDistance + 'px');
+        images.classList.add('scrolling-image');
+
+        selectedImagePosition = focusedImage < Math.floor(imagesPerPage/2) && focusedImage >= 0 ?
+            focusedImage : Math.floor(imagesPerPage/2);
+        console.log('Selected image position', selectedImagePosition);
+    }
+   
     
     document.getElementById(`set_${focusedSet}_image_${focusedImage}`).focus();
+}
+
+function createPlaceholderImage() {
+    const img = document.createElement('input');
+    img.setAttribute('type', 'image');
+    img.classList.add('placeholder-image');
+    img.src = 'images/loop.png';
+    return img;
 }
 
 http.onreadystatechange = (e) => {
@@ -113,15 +163,31 @@ http.onreadystatechange = (e) => {
             responseJSON.data.StandardCollection.containers.forEach((container, index) => {
                 if (container.set?.items) {
                     let setContainer = createSetContainer(container.set.text.title.full.set.default.content, index);
-                    setContainer.appendChild(createSetOptionImagesSection(container.set?.items, /(set_)(\d*)/.exec(setContainer.getAttribute('id'))[2]));
+                    setContainer.appendChild(createSetOptionImagesSection(container.set.items, /(set_)(\d*)/.exec(setContainer.getAttribute('id'))[2]));
                     contentSection.appendChild(setContainer);
-                }
-                
+                }   
             });
         }
     }
    
 }
+
+const observer = new MutationObserver((mutations, obs) => {
+    const firstSelectedImage = document.getElementById(`set_${focusedSet}_image_${focusedImage}`);
+    if (firstSelectedImage) {
+        firstSelectedImage.focus();
+        const setImagesContainer = document.getElementById(`set_${focusedSet}_container`);
+        setImagesContainer.querySelector('.left-arrow').style.display = 'block';
+        setImagesContainer.querySelector('.right-arrow').style.display = 'block';
+        obs.disconnect();
+        return;
+    }
+});
+
+observer.observe(document, {
+    childList: true,
+    subtree: true
+});
 
 const createSetContainer = (setName, index) => {
     const newSet = document.createElement('div');
@@ -166,6 +232,9 @@ const createSetOptionImagesSection = (items, setIndex) => {
     setOptions.classList.add('set-images')
     setImagesContainer.appendChild(setOptions);
     if (items) {
+        const loopBackImage = createPlaceholderImage()
+        setOptions.appendChild(loopBackImage);
+
         let index = 0;
         items.forEach((item) => {
             const tile = item?.image.tile['1.78'];
@@ -182,14 +251,15 @@ const createSetOptionImagesSection = (items, setIndex) => {
                         img.setAttribute('id', `set_${setIndex}_image_${index}`)
                         img.classList.add('preview-image');
                         img.src = itemImageURL
-                        setOptions.appendChild(img);
+                        // setOptions.appendChild(img);
+                        loopBackImage.before(img);
                         index++;
                     } else {
                         console.log('Image does not exist', itemImageURL)
                     }
                 });
             }        
-        })
+        });
     }
     return setImagesContainer;
 }
